a822c3cb2930c637efda777ea503b71509b3beb5
zainahm3d <zainahmed1559@gmail.com>
2018-05-29 20:13:04 -0400

    now a working read example

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,26 @@ int main() {
   can1.frequency(250000);
   can1.mode(CAN::Normal);
 
+  CANMessage msg;
+
   while (1) {
-    if (can1.write(CANMessage(1337, &counter, 1))) {
-      printf("sent\n");
-    } else {
-      printf("error\n");
+    // if (can1.write(CANMessage(1337, &counter, 1))) {
+    //   printf("sent\n");
+    // } else {
+    //   printf("error\n");
+    // }
+    // counter++;
+    // led1 = !led1;
+
+    if (can1.read(msg)) {
+      led1 = !led1;
+      printf("%s", "ID ");
+      printf("%d", msg.id);
+      printf("     ");
+      for (int i = 0; i < 8; i++) {
+        printf("%d ", msg.data[i]);
+      }
+      printf("\n");
     }
-    counter++;
-    led1 = !led1;
-    wait_ms(200);
   }
 }
