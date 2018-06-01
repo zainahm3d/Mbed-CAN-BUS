68203243e0e2d3330d23dad7f9a2c7ce3bbab677
zainahm3d <zainahmed1559@gmail.com>
2018-06-01 13:59:19 -0400

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,27 @@
 CAN can1(PA_11, PA_12);
 DigitalOut led1(LED1);
 char counter = 0;
-int main() {
-  printf("start\n");
 
+int main() {
   can1.frequency(250000);
   can1.mode(CAN::Normal);
-
-  CANMessage msg;
-
+  printf("%s\n", "awake");
   while (1) {
-    // if (can1.write(CANMessage(1337, &counter, 1))) {
-    //   printf("sent\n");
-    // } else {
-    //   printf("error\n");
-    // }
-    // counter++;
+    CANMessage msg;
+    // msg.id = 3;
+    // msg.data[2] = 9;
+    // can1.write(msg);
     // led1 = !led1;
+    // wait_ms(1);
 
     if (can1.read(msg)) {
       led1 = !led1;
       printf("%s", "ID ");
-      printf("%d", msg.id);
+      printf("%d", int(msg.id));
+      printf(" Format ");
+      printf("%d ", msg.format);
       printf("     ");
-      for (int i = 0; i < 8; i++) {
+      for (int i = 0; i < msg.len; i++) {
         printf("%d ", msg.data[i]);
       }
       printf("\n");
