41d72dd1fd2f0086a5918b2241c25043265e27d6
zainahm3d <zainahmed1559@gmail.com>
2018-06-02 15:16:13 -0400

    tested working rpm read out

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,26 @@
 #include "mbed.h"
+
 CAN can1(PA_11, PA_12);
 DigitalOut led1(LED1);
-char counter = 0;
+
+#define RPM_ID 218099784
 
 int main() {
   can1.frequency(250000);
   can1.mode(CAN::Normal);
-  printf("%s\n", "awake");
+
   while (1) {
     CANMessage msg;
-    // msg.id = 3;
-    // msg.data[2] = 9;
-    // can1.write(msg);
-    // led1 = !led1;
-    // wait_ms(1);
-
     if (can1.read(msg)) {
       led1 = !led1;
-      printf("%s", "ID ");
-      printf("%d", int(msg.id));
-      printf(" Format ");
-      printf("%d ", msg.format);
-      printf("     ");
-      for (int i = 0; i < msg.len; i++) {
-        printf("%d ", msg.data[i]);
+
+      if (msg.id == RPM_ID) {
+        int lowByte = msg.data[0];
+        int highByte = msg.data[1];
+        double rpm = ((highByte * 256) + lowByte);
+        printf("%f\n", rpm);
       }
-      printf("\n");
+      //
     }
   }
 }
